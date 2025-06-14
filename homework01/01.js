9ce7159597cf2c70c02772db82a94b7e50007a63
gomteang <gomteang@nate.com>
2025-06-14 17:24:57 +0900

    1번과제 완료

---
+++ homework01/01.js
@@ -0,0 +1,8 @@
+// 1. 인사말 메시지각 항목에 대해 변수나 상수를 선언하고, 알맞은 값을 할당하세요.
+
+// 1. 사용자 이름을 저장할 변수를 선언하고 "자신의 이름"을 할당하세요.
+
+let greetUser = (userName) =>  '안녕하세요!' + userName + '님. 좋은 하루되세요!'
+let massege = greetUser('윤정화')
+
+console.log(massege)
